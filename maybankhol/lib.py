453142190354ac5240d96a5b75eb1000a5d34def
Sebastien Hung <sbyhung@gmail.com>
2021-05-31 13:28:50 +0100

    initial push

---
+++ maybankhol/lib.py
@@ -0,0 +1,5 @@
+def try_me():
+    print('this has to be the right one...')
+
+if __name__ == "__main__":
+    print(try_me())
---
+++ yourpackage/lib.py
@@ -0,0 +1,5 @@
+def try_me():
+    print('try me, try you...')
+
+if __name__ == "__main__":
+    print(try_me())
